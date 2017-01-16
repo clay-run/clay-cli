ae40f087be137a0732bbbcab0d7fc3c1a3a22acf
Kareem Amin <kareem@useframe.com>
2017-01-16 16:59:05 -0500

    better help message after creating service

--- src/new-service.js
+++ src/new-service.js
@@ -19,11 +19,20 @@ module.exports = function(serviceName) {
    ,    SERVICE_NOT_CREATED          = chalk.white("Service was not created. Contact support@tryclay.com") + chalk.white(`\nCleaning up any files or directories that were created`)
    ,    SERVICE_CREATED_MSG          = `
 ${chalk.white('Your node service is now live and deployed.\n')}
+${chalk.white('URL FOR SERVICE')}
+${chalk.white('******************************************\n')}
+${chalk.red('%s\n')}
+${chalk.white('EDIT CODE AND CONFIGURE SERVICE')}
+${chalk.white('******************************************\n')}
 ${chalk.white('The next step is to edit the code and configuration for your service which is here:') + '\n' + chalk.red('%s\n')}
-${chalk.white('After you make changes test it locally using\n') +  chalk.red('clay test \n')}
-${chalk.white('Then you can deploy it to production using\n') +  chalk.red('clay deploy \n')}
+${chalk.white('After you make changes test it locally using:\n') +  chalk.red('clay test \n')}
+${chalk.white('DEPLOY YOUR SERVICE')}
+${chalk.white('******************************************\n')}
+${chalk.white('Then you can deploy it to production using:\n') +  chalk.red('clay deploy \n')}
+${chalk.white('RUN YOUR SERVICE:')}
+${chalk.white('******************************************\n')}
 ${chalk.white('You can run your service by using the visual interface or by making an HTTP POST request to following url from your code:')}
-${chalk.red('%s/%s/%s\n')}
+${chalk.red('%s\n')}
 ${chalk.white('You can also run it on production using:\n') +  chalk.red('clay run \n')}
 ${chalk.white("That's all there is to it! For more information and help go to")+chalk.red(' http://www.github.com/clay-run/clay-cli')} `;
 
@@ -92,7 +101,8 @@ ${chalk.white("That's all there is to it! For more information and help go to")+
   // Set the directory to act on as the new service directory
   this.deploy({mode: 'POST', dir: dir})
   .then((deployResponse) => {
-    print(SERVICE_CREATED_MSG, dir, this.apis.servicePage, this.credentials.username, serviceName);
+    var urlForService = `${this.apis.servicePage}/${this.credentials.username}/${serviceName}`
+    print(SERVICE_CREATED_MSG, urlForService, dir, urlForService);
   })
   .catch((err) => {
     if(err.statusCode == 409) print(SERVICE_EXISTS_ERR_MSG)
