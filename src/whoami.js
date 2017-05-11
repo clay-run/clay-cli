88a46108d663b4b1fba5b84845e64ecef9f8e295
Kareem Amin <kareem@useframe.com>
2017-05-10 20:57:07 -0400

    updating whoami to print the correct output

--- src/whoami.js
+++ src/whoami.js
@@ -1,7 +1,6 @@
 const  chalk         = require('chalk');
 
 module.exports = function() {
-  // Pretty print the config here
   console.log(chalk.white(`You are currently signed in as: `)+chalk.red(`${this.credentials.username}\n`));
 }
 
