78ef1adee8e483339da9eb3525cb88ab8241ce00
Théo Szymkowiak <theoszymko@me.com>
2017-05-23 11:17:41 -0400

    Replacing native zip command by archiver package

--- src/deploy-service.js
+++ src/deploy-service.js
@@ -3,9 +3,9 @@ var path                      = require('path')
  ,  chalk                     = require('chalk')
  ,  rp                        = require('request-promise-native')
  ,  print                     = console.log
- ,  macCommand                = 'zip -r  - node_modules * | base64'
+ ,  archiver                  = require('archiver')
  ,  UPDATING_SERVICE_MSG      = chalk.white(`Updating Service...\n`)
- ,  SERVICE_UPDATED_MSG       = chalk.white(`Service Updated.`)
+ ,  SERVICE_UPDATED_MSG       = chalk.green(`✅ Service Updated.`)
  ,  SERVICE_UPDATE_FAILED_MSG = chalk.white(`Service failed to update. Please contact support@clay.run`)
  ,  clui                      = require('clui')
  ,  Spinner                   = clui.Spinner;
@@ -16,23 +16,37 @@ module.exports = function(deployConfig) {
     const dir                     = deployConfig.dir
     const currentProjectConfig    = require(path.resolve(dir,  'clay-config.json'));
     const USER_NOT_AUTHORIZED_ERR = chalk.white(`Current user is not authorized to create or update services. You are signed as: `)+chalk.red(`${this.credentials.username}\n`)
-    const SERVICE_URL_MSG         = chalk.white(`Your service is available here: `)+chalk.red(`${this.apis.servicePage}/${this.credentials.username}/${currentProjectConfig.serviceName}`)
+    const SERVICE_URL_MSG         = chalk.white(`🚀 Your service is available here: `)+chalk.green.underline(`${this.apis.servicePage}/${this.credentials.username}/${currentProjectConfig.serviceName}`)
 
     var execOptions = {
       maxBuffer: 1024 * 50000,
       cwd: dir
     };
 
-    if(deployConfig.mode == 'PUT' && !deployConfig.suppressProgressMessages) print(UPDATING_SERVICE_MSG)
-
     var status = new Spinner('Building your service..');
     status.start();
+                  
+    var archive = archiver('zip');
 
-    exec(macCommand, execOptions, (err, stdout) => {
+    // Adding the function directory
+    archive.directory('.', true, { date: new Date() });
+
+    // On zipping error
+    archive.on('error', function(err) {    
       if (err) {
         print(SERVICE_UPDATE_FAILED_MSG)
         return
       }
+    });
+
+    // Saving buffers to RAM
+    var zip_buffers = [];
+    archive.on('data', function(buffer) {
+        zip_buffers.push(buffer);
+    });
+
+    archive.on('end', function() {
+      var zip_buffer = Buffer.concat(zip_buffers); 
 
       var requestOptions = {
         uri: this.apis.methodsApi,
@@ -44,7 +58,7 @@ module.exports = function(deployConfig) {
           function_input: JSON.stringify(currentProjectConfig.inputs),
           apiToken: this.credentials.token,
           serviceType: currentProjectConfig.serviceType,
-          fileData: stdout
+          fileData: zip_buffer.toString('base64')
         },
         timeout: 0,
         json: true
@@ -54,6 +68,7 @@ module.exports = function(deployConfig) {
       rp(requestOptions)
       .then((response) => {
         status.stop();
+        console.log(('Your service weights ' + archive.pointer() + ' bytes.'));
         if(response.result == true && deployConfig.mode  == 'PUT') {
           var time = new Date();
           if(!deployConfig.suppressProgressMessages) {
@@ -70,7 +85,9 @@ module.exports = function(deployConfig) {
         else if(deployConfig.mode == 'PUT') print(SERVICE_UPDATE_FAILED_MSG)
         reject(err);
       })
-    })
+    }.bind(this))
+
+    archive.finalize();
   })
 
 }
