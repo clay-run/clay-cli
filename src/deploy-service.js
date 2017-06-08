b34d6f54975f723b74e8b8c289447f11dde3e3b4
Théo Szymkowiak <theoszymko@me.com>
2017-06-08 14:34:39 -0400

    Config linting

--- src/deploy-service.js
+++ src/deploy-service.js
@@ -23,74 +23,82 @@ module.exports = function(deployConfig) {
       cwd: dir
     };
 
-    var status = new Spinner('Building your service..');
+    var status = new Spinner('Verifying config file');
     status.start();
-    var archive = archiver('zip');
 
-    // Adding the function directory
-    archive.directory(deployConfig.dir || '.', false, { date: new Date() });
+    this.lintConfig(deployConfig.dir).then(function() {
+    process.exit(1);
 
-    // On zipping error
-    archive.on('error', function(err) {
-      if (err) {
-        print(SERVICE_UPDATE_FAILED_MSG)
-        return
-      }
-    });
+        status.message('Building your service..');
+        var archive = archiver('zip');
 
-    // Saving buffers to RAM
-    var zip_buffers = [];
-    archive.on('data', function(buffer) {
-        zip_buffers.push(buffer);
-    });
+        // Adding the function directory
+        archive.directory(deployConfig.dir || '.', false, { date: new Date() });
 
-    archive.on('end', function() {
-      var zip_buffer = Buffer.concat(zip_buffers);
-
-      var requestOptions = {
-        uri: this.apis.methodsApi,
-        method: deployConfig.mode ,
-        body: {
-          commandDescription: currentProjectConfig.serviceDescription,
-          methodDisplayName: currentProjectConfig.serviceDisplayName,
-          commandName: currentProjectConfig.serviceName,
-          function_input: JSON.stringify(currentProjectConfig.inputs),
-          apiToken: this.credentials.token,
-          serviceType: currentProjectConfig.serviceType,
-          fileData: zip_buffer.toString('base64')
-        },
-        timeout: 0,
-        json: true
-      }
-
-      status.message('Deploying ' + currentProjectConfig.serviceDisplayName + ' on Clay Cloud..');
-      rp(requestOptions)
-      .then((response) => {
-        status.stop();
-        var bytes = archive.pointer();
-        var mbs = Math.floor(((bytes / 1024) / 1024) * 100) / 100;
-        console.log(('Your service weights ' + archive.pointer() + ' bytes (' + mbs + ' MBs).'));
-        if(response.result == true && deployConfig.mode  == 'PUT') {
-          var time = new Date();
-          if(!deployConfig.suppressProgressMessages) {
-            print(SERVICE_UPDATED_MSG, time.toLocaleDateString(), time.toLocaleTimeString())
-          }
-          print(SERVICE_URL_MSG)
+        // On zipping error
+        archive.on('error', function(err) {
+        if (err) {
+            print(SERVICE_UPDATE_FAILED_MSG)
+            return
         }
-          resolve(response);
-      })
-      .catch((err) => {
+        });
+
+        // Saving buffers to RAM
+        var zip_buffers = [];
+        archive.on('data', function(buffer) {
+            zip_buffers.push(buffer);
+        });
+
+        archive.on('end', function() {
+        var zip_buffer = Buffer.concat(zip_buffers);
+
+        var requestOptions = {
+            uri: this.apis.methodsApi,
+            method: deployConfig.mode ,
+            body: {
+            commandDescription: currentProjectConfig.serviceDescription,
+            methodDisplayName: currentProjectConfig.serviceDisplayName,
+            commandName: currentProjectConfig.serviceName,
+            function_input: JSON.stringify(currentProjectConfig.inputs),
+            apiToken: this.credentials.token,
+            serviceType: currentProjectConfig.serviceType,
+            fileData: zip_buffer.toString('base64')
+            },
+            timeout: 0,
+            json: true
+        }
+
+        status.message('Deploying ' + currentProjectConfig.serviceDisplayName + ' on Clay Cloud..');
+        rp(requestOptions)
+        .then((response) => {
+            status.stop();
+            var bytes = archive.pointer();
+            var mbs = Math.floor(((bytes / 1024) / 1024) * 100) / 100;
+            console.log(('Your service weights ' + archive.pointer() + ' bytes (' + mbs + ' MBs).'));
+            if(response.result == true && deployConfig.mode  == 'PUT') {
+            var time = new Date();
+            if(!deployConfig.suppressProgressMessages) {
+                print(SERVICE_UPDATED_MSG, time.toLocaleDateString(), time.toLocaleTimeString())
+            }
+            print(SERVICE_URL_MSG)
+            }
+            resolve(response);
+        })
+        .catch((err) => {
+            status.stop();
+            if(process.env.CLAY_DEV) console.log(err);
+            if(err.statusCode == 401) print(USER_NOT_AUTHORIZED_ERR)
+            else if(deployConfig.mode == 'PUT') print(SERVICE_UPDATE_FAILED_MSG)
+            reject(err);
+        })
+        }.bind(this))
+
+        archive.finalize();
+    }, function(errors) {
         status.stop();
-        if(process.env.CLAY_DEV) console.log(err);
-        if(err.statusCode == 401) print(USER_NOT_AUTHORIZED_ERR)
-        else if(deployConfig.mode == 'PUT') print(SERVICE_UPDATE_FAILED_MSG)
-        reject(err);
-      })
-    }.bind(this))
-
-    archive.finalize();
+        print('Could not deploy function');
+    });
   })
-
 }
 
 
---
+++ src/lint-config.js
@@ -0,0 +1,87 @@
+var chalk               = require('chalk')
+  , print               = console.log
+  , path                = require('path')
+  , _                   = require('underscore');
+
+module.exports = function(configDir) {
+    return new Promise((resolve, reject) => {
+        const config     = require(path.resolve(configDir,  'clay-config.json'));
+        var   errors     = []
+            , validTypes = ['db', 'text', 'url', 'date', 'address', 'select', 'image', 'file', 'json', 'autocomplete'];
+
+        if(!config.serviceName || config.serviceName.length < 3) {
+            errors.push({
+                message: 'Please provide a ' + chalk.underline('`serviceName`.')
+            })
+        }
+
+        if(!config.serviceDisplayName || config.serviceDisplayName.length < 3) {
+            errors.push({
+                message: 'Please provide a ' + chalk.underline('`serviceDisplayName`.')
+            })
+        }
+
+        if(!config.serviceDescription || config.serviceDescription.length < 3) {
+            errors.push({
+                message: 'Please provide a ' + chalk.underline('`serviceDescription`.')
+            })
+        }
+
+        if(config.inputs) {
+            var index = 0;
+            var namesTaken = {};
+
+            _.each(config.inputs, function(input) {
+                index++;
+
+                if(!input.name || input.name.length < 2) {
+                    errors.push({
+                        message: 'Input #'+index+' needs a name (at least 1 character).'
+                    });
+                }
+
+                var name = input.name;
+                if(namesTaken[name]) {
+                    errors.push({
+                        message: 'Duplicate name '+chalk.green(name)+' for input #'+index
+                    })
+                }
+                namesTaken[name] = true;
+
+                if(!input.type) {
+                    errors.push({
+                        message: 'Input '+chalk.green(name)+' does not have a valid type.'
+                    })
+                };
+
+                if(!_.contains(validTypes, input.type)) {
+                    errors.push({
+                        message: 'Type ' + chalk.green(input.type) + ' is not valid for input ' + chalk.green(name)
+                    })
+                }
+
+                if(!input.displayName || input.name.displayName < 2) {
+                    errors.push({
+                        message: 'Input ' + chalk.green(name) + ' does not have a description (at least 1 character)'
+                    })
+                }
+            });      
+        }
+
+        if(errors.length == 0) {
+            resolve();
+        } else {
+            // Print errors
+            print(chalk.bold('\n⛔️ clay-config.js contains ' + chalk.red(errors.length) + ' error'+ (errors.length > 1 ? 's' : '') +':'));
+            var i = 0;
+            _.each(errors, function(error) {
+                print(chalk.red('- #'+i+' ') + error.message);
+                i++;
+            })
+
+            print('\n');
+
+            reject(errors);
+        }
+    });
+}
